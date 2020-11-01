6e83331d57f1a954911014edae5dec1ae4bdab99
ana-mata <ana.mata7892@gmail.com>
2020-11-01 03:11:25 -0700

    Removed unused locks

--- Queue.py
+++ Queue.py
@@ -1,25 +1,20 @@
 import queue
-from threading import Lock, Semaphore
+from threading import Semaphore
 
 class ThreadyQueue:
     def __init__(self, maxcount):
         self.base_q = queue.Queue()
-        self.lock = Lock()
         self.queue_spots = Semaphore(maxcount)
         self.not_empty = Semaphore(0)
     
     def put(self, item):
         self.queue_spots.acquire()
-        self.lock.acquire()
         self.base_q.put(item)
-        self.lock.release()
         self.not_empty.release()
 
     def get(self):
         self.not_empty.acquire()
         self.queue_spots.release()
-        self.lock.acquire()
         item = self.base_q.get()
-        self.lock.release()
         
         return item
