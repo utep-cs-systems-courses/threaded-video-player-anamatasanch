14daec34c01a0970c82f224afa5d28c130ca3a6c
ana-mata <ana.mata7892@gmail.com>
2020-11-01 03:31:42 -0700

    Added locks after all

--- Queue.py
+++ Queue.py
@@ -1,20 +1,25 @@
 import queue
-from threading import Semaphore
+from threading import Lock, Semaphore
 
 class ThreadyQueue:
     def __init__(self, maxcount):
         self.base_q = queue.Queue()
+        self.lock = Lock()
         self.queue_spots = Semaphore(maxcount)
         self.not_empty = Semaphore(0)
     
     def put(self, item):
         self.queue_spots.acquire()
+        self.lock.acquire()
         self.base_q.put(item)
+        self.lock.release()
         self.not_empty.release()
 
     def get(self):
         self.not_empty.acquire()
         self.queue_spots.release()
+        self.lock.acquire()
         item = self.base_q.get()
+        self.lock.release()
         
         return item
